b312ff3dedfabc1a4367b177a353b569f2892aa5
Jonathan Shieh <jonathan.shieh@gmail.com>
2024-01-05 16:18:42 +0800

    Disable default nvchad keymaps

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -77,8 +77,10 @@ M.load_mappings = function(section, mapping_opt)
     local mappings = require("core.utils").load_config().mappings
 
     if type(section) == "string" then
-      mappings[section]["plugin"] = nil
-      mappings = { mappings[section] }
+      if mappings[section] then
+        mappings[section]["plugin"] = nil
+        mappings = { mappings[section] }
+      end
     end
 
     for _, sect in pairs(mappings) do
